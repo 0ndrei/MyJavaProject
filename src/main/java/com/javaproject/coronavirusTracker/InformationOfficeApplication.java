1bc5d3c3802b7e34448929b2f5043fa363bef8fd
0ndrei <lungu.andrei.00@mail.ru>
2021-09-25 19:25:48 +0300

    I made changes in the class CoronaVDataService

--- src/main/java/com/javaproject/coronavirusTracker/InformationOfficeApplication.java
+++ src/main/java/com/javaproject/coronavirusTracker/InformationOfficeApplication.java
@@ -2,8 +2,10 @@ package com.javaproject.coronavirusTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class InformationOfficeApplication {
 
 	public static void main(String[] args) {
