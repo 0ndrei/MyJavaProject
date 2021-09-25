d207b8dc230d8533bf0dfe860946c48900fc0dc7
0ndrei <lungu.andrei.00@mail.ru>
2021-09-25 18:02:59 +0300

    Initial commit

---
+++ src/main/java/com/javaproject/coronavirusTracker/InformationOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.coronavirusTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/coronavirusTracker/InformationOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.coronavirusTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
