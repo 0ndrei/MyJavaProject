163f7e4686993725262322aa0382a6589267ba65
0ndrei <lungu.andrei.00@mail.ru>
2021-09-26 11:06:44 +0300

    I created the data table

--- src/main/java/com/javaproject/coronavirusTracker/controllers/HomeController.java
+++ src/main/java/com/javaproject/coronavirusTracker/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package com.javaproject.coronavirusTracker.controllers;
 
+import com.javaproject.coronavirusTracker.services.CoronaVDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    CoronaVDataService coronaVDataService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("locationStats", coronaVDataService.getAllStats());
         return "home";
     }
 }
