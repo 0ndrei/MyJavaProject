86017b54619dd3176eb56e10262c68dd86b012b5
Lungu Andrei <54952551+0ndrei@users.noreply.github.com>
2021-10-02 16:43:15 +0300

    Delete CoronaVirusInfo directory

--- CoronaVirusInfo/src/main/java/com/example/CoronaVirusInfo/controller/CoronaVController.java
+++
@@ -1,5 +0,0 @@
-package com.example.CoronaVirusInfo.controller;
-
-public class CoronaVController {
-
-}
--- CoronaVirusInfo/src/main/java/com/example/CoronaVirusInfo/repository/CoronaVirusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CoronaVirusInfo.repository;
-
-import com.example.CoronaVirusInfo.models.CoronaData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoronaVirusRepository extends JpaRepository<CoronaData, Long>  {
-}
